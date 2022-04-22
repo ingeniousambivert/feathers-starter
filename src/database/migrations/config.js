e61a16ed8677536196acab8d82e6273c62393370
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2022-04-22 23:14:53 +0530

    Update config.js

--- src/database/migrations/config.js
+++ src/database/migrations/config.js
@@ -1,8 +1,17 @@
+/* eslint-disable no-dupe-else-if */
 const app = require("../../app");
 const env = process.env.NODE_ENV || "development";
 const dialect = "postgres";
 const postgresConfig = app.get(dialect);
-const connectionString = `postgres://${postgresConfig.user}:${postgresConfig.password}@${postgresConfig.ip}:${postgresConfig.port}/${postgresConfig.db}`;
+
+let connectionString = `postgres://${postgresConfig.ip}:${postgresConfig.port}/${postgresConfig.db}`;
+
+if (postgresConfig.user) {
+	connectionString = `postgres://${postgresConfig.user}@${postgresConfig.ip}:${postgresConfig.port}/${postgresConfig.db}`;
+} else if (postgresConfig.user && postgresConfig.password) {
+	connectionString = `postgres://${postgresConfig.user}:${postgresConfig.password}@${postgresConfig.ip}:${postgresConfig.port}/${postgresConfig.db}`;
+}
+
 module.exports = {
 	[env]: {
 		dialect,
